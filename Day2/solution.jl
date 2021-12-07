53f4008eda22161985e721c4c42e466e0b3b5a32
emiliohdez <emiliohdezhfno@gmail.com>
2021-12-07 11:36:14 -0600

    Added solution for day 2 (both parts)

---
+++ Day2/solution.jl
@@ -0,0 +1,32 @@
+input = readlines(joinpath(@__DIR__, "input.txt"));
+
+# PART 1
+# position = [0, 0]
+# commands = split.(input, " ")
+# for command in commands
+#     if command[1] == "forward"
+#         position[1] += parse(Int64, command[2])
+#     elseif command[1] == "down"
+#         position[2] += parse(Int64, command[2])
+#     elseif command[1] == "up"
+#         position[2] -= parse(Int64, command[2])
+#     end
+# end
+# println(position[1] * position[2])
+
+# PART 2
+position = [0, 0]
+aim = 0
+commands = split.(input, " ")
+for command in commands
+    X = parse(Int64, command[2])
+    if command[1] == "forward"
+        position[1] += X
+        position[2] += aim * X
+    elseif command[1] == "down"
+        global aim += X
+    elseif command[1] == "up"
+        global aim -= X
+    end
+end
+println(position[1] * position[2])
